0140b3cfacf2426aa2a79f5040c80c0b0cdb6d40
Jeremy Flowers <flowersje08@gmail.com>
2020-06-26 23:15:08 -0700

    added routes and db variable to server.js

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
 
-const PORT = process.env.PORT || 3000
+//require the models folder - was getting a "db is not defined" error from line 46
+const db = require("./models");
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -11,8 +14,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// make sure to change "budget" below to be the right database
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+// make sure to change "budget" below to be the right database - according to seed.js, the database will be called workout
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
@@ -29,8 +32,36 @@ app.get('/exercise', function (req, res) {
   });
 
 // send stats.html when the /stats api route is called
-  app.get('/stats', function (req, res) {
+app.get('/stats', function (req, res) {
     res.sendFile(path.join(__dirname,'./public/stats.html'))
   });
 
+  // app.get('/api/workouts', function (req, res) {
+  //   // Find all data in a Collection with `db.[COLLECTION_NAME].find()`.
+  //   const workouts = db.workouts.find();
+  //   console.log(workouts);
+  //   //this syntax is a total guess
+  //   res.send(res.json(workouts));
+  // })
   
+//write an app.get for "/api/workouts" from api.js
+app.get("/api/workouts", (req, res) => {
+  db.workouts.find({}, (err, found) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(found);
+    }
+  });
+});
+
+app.post('/api/workouts', function (req, res) {
+  //take in json.stringify data
+  console.log("post /api/workouts req" + req.body);
+  console.log("post /api/workouts res" + res.body); 
+})
+
+app.put('/api/workouts', function (req, res) {
+  console.log('put /api/workouts req: ' + req.body);
+  console.log('put /api/workouts res: ' + res.body);
+})
